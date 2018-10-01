87717f8119c337f69744bb0a38f051fb6951aa4d
Daniel Nicoletti <dantti12@gmail.com>
2018-10-01 16:24:34 -0300

    Fixes code injection for cms_foot
    Fix: #14

--- src/root.cpp
+++ src/root.cpp
@@ -95,7 +95,7 @@ void Root::page(Cutelyst::Context *c)
         c->setStash(QStringLiteral("cms_head"), QVariant::fromValue(safe));
     }
 
-    const QString cms_foot = settings.value(QStringLiteral("cms_head"));
+    const QString cms_foot = settings.value(QStringLiteral("cms_foot"));
     if (!cms_foot.isEmpty()) {
         const Grantlee::SafeString safe(cms_foot, true);
         c->setStash(QStringLiteral("cms_foot"), QVariant::fromValue(safe));
